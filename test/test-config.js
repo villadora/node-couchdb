e4257b79aa26eea3cc71a6fdf6f39a32efe420e9
Wei Gao <jky239@gmail.com>
2014-04-02 18:58:22 +0800

    change way passing the url

--- test/test-config.js
+++ test/test-config.js
@@ -1,7 +1,6 @@
 module.exports = {
-	"url":"https://skimdb.npmjs.com/"
+//bin/	"url":"https://skimdb.npmjs.com/"
     // "url": "http://isaacs.iriscouch.com"
-    "url": "http://127.0.0.1:15984",
-    "user": "admin",
-    "pass": "admin"
+    "url": "http://127.0.0.1:5984",
+    "user": ""
 };
